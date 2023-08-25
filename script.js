5181a082c887b2ae0faf87403d48ff945a42b77e
Shreyas Shinde <130788791+shreyasShinde1@users.noreply.github.com>
2023-08-25 20:29:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const div = document.createElement("div")
+div.id = "browser-info"
+
+let browserName = navigator.appName;
+let version = navigator.appVersion;
+div.innerText = "You are Using "+browserName + " version "+ version;
+
+document.body.appendChild(div);
